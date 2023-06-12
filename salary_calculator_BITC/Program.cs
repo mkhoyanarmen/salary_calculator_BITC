40fb866cb921337b8c10e58c9cf3c52674811866
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-12 21:36:27 +0400

    Add project files.

---
+++ salary_calculator_BITC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace salary_calculator_BITC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal salary = 200000;
+            decimal incomeTax_per = 20;
+            decimal stampDuty = 3000;
+            decimal socialFee = 10000;
+            Console.WriteLine($"Salary: {salary}");
+
+            salaryCalculator(ref salary, ref incomeTax_per, socialFee, stampDuty);
+
+            Console.WriteLine($"Income Tax: {incomeTax_per}");
+            Console.WriteLine($"Stamp duty: {stampDuty}");
+            Console.WriteLine($"Social fee: {socialFee}");
+            Console.WriteLine($"\nClean Balance: {salary}");
+        }
+        static bool salaryCalculator(ref decimal salary, ref decimal incomeTax_per, in decimal socialFee, decimal stampDuty)
+        {
+            incomeTax_per = salary * (incomeTax_per / 100);
+            salary -= incomeTax_per + socialFee + stampDuty;
+            return true;
+        }
+    }
+}
